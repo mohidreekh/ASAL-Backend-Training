3a6d188d35adb830594ff3416719b38209ab9a5f
mohidreekh <mohammad.idreekh00@gmail.com>
2026-07-30 15:52:55 +0300

    fix the solution of problem 5

--- problem-5.cpp
+++ problem-5.cpp
@@ -4,16 +4,20 @@
 
 using namespace std;
 
-int main() 
+int main()
 {
-	//vector<vector<int>> triangle = { {2} ,{3,4},{6,5,7},{4,1,8,3} };
-	vector<vector<int>> triangle = { {-10} };
+	vector<vector<int>> triangle = { {2} ,{3,4},{6,5,7},{4,1,8,3} };
+	//vector<vector<int>> triangle = { {-10} };
 
-	int sum = 0;
-	for (int i = 0; i < triangle.size(); i++)
+	int sum = triangle[0][0];
+	vector<int> dp = triangle.back();
+
+	for (int i = triangle.size() - 2; i >= 0; i--)
 	{
-		int minValue = *min_element(triangle[i].begin(), triangle[i].end());
-		sum += minValue;
+		for (int j = 0; j <= i; j++)
+		{
+			dp[j] = triangle[i][j] + min(dp[j], dp[j + 1]);
+		}		
 	}
-	cout << sum << endl;
+	cout << dp[0] << endl;
 }
